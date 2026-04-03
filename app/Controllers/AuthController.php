c023dd4e97077fe36136b1104f86a7972bdf74d0
Quy Dev <quydev@1pixel.vn>
2026-04-03 20:05:25 +0700

    fix(auth): resolve fatal inheritance error for userModel property and disable debug mode

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -7,11 +7,9 @@ use App\Models\UserModel;
 
 class AuthController extends BaseController {
     protected $requireAuth = false;
-    private $userModel;
 
     public function __construct() {
         parent::__construct();
-        $this->userModel = new UserModel();
     }
 
     /**
